cfa9716e8bc3a717466517693bae61724eef4df4
skadakar <dale.ole.morten@gmail.com>
2020-10-17 05:46:26 +0200

    attempt to remove empty folder

---
+++ bbcalc.py
@@ -0,0 +1,46 @@
+# This app is to figure out how much residual energy is in a projectile after a certain range given some basic parameters.
+# Two versions will be available, one with more input variables and one basic for general usage.
+# We do not account for the magnus effect for BBs with backspin nor do we account for humidity. 
+# Real world results will always be lower than this, but this will give you the theoretical max.
+#
+# Remainder with all variables and remainder with commandline variables might be added.
+#
+# Assumed values is the default CQB legal limits for airsoft in norway. 
+#energy_j = 0.9
+#weight_g = 0.2
+#weight_kg = weight_g/1000
+#dragcoefficient = 0.47 # Assuming a circular projectile
+#airdensity_kgm3 = 1.225 # 0 humidity at 0m above sealevel
+#diameter_mm = 6 # 6mm 
+#radius_m = diameter_mm / 1000 / 2
+#crossection_m2 = math.pi*((radius_m)**2)
+#distance_m = 30
+#speed_ms = math.sqrt(energy_j/(0.5*weight_kg))
+
+import math
+
+def residual_simple(energy_j,weight_g,distance_m):
+    weight_kg = float(weight_g) / 1000
+    dragcoefficient = float(0.47)
+    airdensity_kgm3 = float(1.225)
+    diameter_mm = int(6)
+    radius_m = diameter_mm / 1000 / 2
+    crossection_m2 = math.pi*((float(radius_m))**2)
+    speed_ms = math.sqrt(float(energy_j)/(0.5*float(weight_kg)))
+    step1 = airdensity_kgm3 * crossection_m2 * dragcoefficient
+    step2 = 2 * weight_kg 
+    result= speed_ms *math.exp(-((step1)/(step2)*int(distance_m)))
+    return result
+
+
+# Get user inputs
+energy = input("Energy at muzzle in joule ")
+weight = input("Projectile weight in grams ")
+distance = input("Distance in meters ")
+
+# Send off the input to the function get_rema
+get_residual = residual_simple(energy,weight,distance)
+
+# Print actual results
+print("Maximum potential residual energy at " + str(distance) + " meters with " + str(weight) + " projectiles.")
+print(str(round(get_residual,2)) + " J")
